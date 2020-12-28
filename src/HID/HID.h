19469dfe388f141e9ffffc48b41d0fbbf9297d95
abratchik <alexbratchik@yandex.com>
2020-12-28 18:25:58 +0400

    minor cosmetics

--- src/HID/HID.h
+++ src/HID/HID.h
@@ -1,6 +1,7 @@
 /*
   Copyright (c) 2015, Arduino LLC
   Original code (pre-library): Copyright (c) 2011, Peter Barrett
+  Modified code: Copyright (c) 2020, Aleksandr Bratchik
 
   Permission to use, copy, modify, and/or distribute this software for
   any purpose with or without fee is hereby granted, provided that the
